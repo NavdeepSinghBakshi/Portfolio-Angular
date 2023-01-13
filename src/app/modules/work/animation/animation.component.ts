5547176ed9b51b4ae2a545f42f6234cd9ee4e8dc
navdeep <navdeepbakshi47@gmail.com>
2023-01-14 01:18:45 +0530

    animation added

--- src/app/modules/work/animation/animation.component.ts
+++ src/app/modules/work/animation/animation.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animation.component.css']
 })
 export class AnimationComponent implements OnInit {
-
+  AnimationState : boolean = true
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  pauseAnimation() {
+    let s = document.getElementById('card') as HTMLElement
+    s.style.animationPlayState = 'paused';
+    this.AnimationState = false;
+  }
+  startAnimation() {
+    let s = document.getElementById('card') as HTMLElement
+    s.style.animationPlayState = 'running';
+    this.AnimationState = true;
+  }
 }
